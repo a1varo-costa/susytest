c9e198f38110d7124a79661f25e6edbb509da48d
Álvaro A. Costa <alvaroc.dev@gmail.com>
2021-06-19 17:16:11 -0300

    Add output diff

--- susytest.py
+++ susytest.py
@@ -1,5 +1,6 @@
 from urllib.request import urlopen
 import subprocess
+import difflib
 import pprint
 import click
 import ssl
@@ -71,6 +72,7 @@ def download_tests(url):
 
 
 def run(cmd, _in):
+    """Run `cmd` passing `_in` to stdin"""
     proc = subprocess.run(
         args            = cmd,
         input           = ''.join(_in),
@@ -80,6 +82,23 @@ def run(cmd, _in):
         timeout         = 1.5    # seconds
     )
     return proc
+
+
+def pretty_diff_ans(a, b):
+    """Compare `a` and `b` returning a list of ANSI styled string
+    and a boolean indicating if `a` and `b` are equal"""
+    d = difflib.ndiff(a, b)
+    ret = []
+    equal = True
+    for s in d:
+        if s.startswith('- '):
+            ret.append(click.style(s, fg='red'))
+        elif s.startswith('? '):
+            equal = False
+            ret.append(click.style(s, fg='cyan'))
+        else:
+            ret.append(click.style(s, fg='green'))
+    return ret, equal
 
 
 @click.command()
@@ -106,20 +125,44 @@ def cli(url, prog, nodiff, verbose):
 
     for test in tests:
         try:
-            click.echo(f">> TEST {test['id']}")
+            click.echo(click.style(f">> TEST {test['id']}", fg='yellow'))
             result = run(prog, test['in'])
 
             if verbose:
                 click.echo(
-                    f'> Input:\n' + pp.pformat(test['in'])
+                    click.style('> Input:', bold=True) +
+                    '\n' +
+                    pp.pformat(test['in'])
                 )
                 click.echo(
-                    f'> Output:\n' + pp.pformat(result.stdout.splitlines(keepends=True))
+                    '\n' +
+                    click.style('> Output:', bold=True) +
+                    '\n' +
+                    pp.pformat(result.stdout.splitlines(keepends=True)) +
+                    '\n'
                 )
+
+            diff, equal = pretty_diff_ans(
+                result.stdout.splitlines(keepends=True),
+                test['sol']
+            )
+
+            if nodiff:
+                msg = 'OK' if equal else 'Wrong Answer'
+                fg = 'green' if equal else 'red'
+                click.secho('> ' + msg, fg=fg)
+                continue
+
+            click.echo(
+                click.style('> Diff:', bold=True) +
+                '\n' +
+                ''.join(diff)
+            )
         except subprocess.TimeoutExpired as e:
             click.echo(f'> Timeout of {e.timeout}s expired while waiting for program "{e.cmd}".')
         except subprocess.CalledProcessError as e:
             click.echo(f'> Program "{e.cmd}" returned non-zero exit status {e.returncode}.')
-        except:
-            click.echo(f'> Unexpected error. Aborting...')
+        except Exception as e:
+            click.echo('> Unexpected error:\n')
+            click.echo(e)
             sys.exit(1)
